6183af5743df0b96227fdf3d84588996c0e79f24
KU <vebvdev@gmail.com>
2023-05-03 12:42:47 +0900

    First commit. Java project practice 01

---
+++ src/hi/org/ConsoleOutput.java
@@ -0,0 +1,23 @@
+package hi.org;
+
+public class ConsoleOutput {
+
+	public static void main(String[] args) {
+		String hi[] = {"hi","hello"};
+		System.out.println(hi[0]+" , "+hi[1]);
+		System.out.println(2 + 4);
+		System.out.println("2" + "4");
+		System.out.println(11.5 + 11.5);
+		System.out.println("11.5" + 11.5); //실수를 문자열로 처리
+		
+		//변수 : 문자열
+		String str = "String type var" ;
+		System.out.println(str);
+		//변수 : 정수
+		int num1 = 10 ;
+		System.out.println(num1);
+		//변수 : 실수
+
+	}
+
+}
---
+++ src/hi/org/HelloWorld.java
@@ -0,0 +1,10 @@
+package hi.org;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.print("Hello\n"); // 출력
+		System.out.println("Hello World"); //출력 + 개행
+	}
+
+}
